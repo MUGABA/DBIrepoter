2e772de7428028fb6290ad25320c00b647d290f2
MUGABA <mugabamuha@gmail.com>
2019-06-05 14:26:52 +0300

    heroku deployment

--- Instance/config.py
+++ Instance/config.py
@@ -23,7 +23,15 @@ class Testingconfig(Baseconfig):
 	DATABASE_URI = 'postgres://postgres:rashid123@localhost:5432/test_ireporter'
 
 
+class Productionconfig(Baseconfig):
+	DEBUG = False
+	Testing = False
+	ENV = 'Production'
+	DATABASE_URI = 'postgres://cwzwwmybfdnund:c88bb6f2d5ef6e19aea84410009e78196de78219036e89c5b6ba5248c91b103b@ec2-54-83-192-245.compute-1.amazonaws.com:5432/d9b83nf6dba47b'
+
+
 app_config = {
 	"Development":Developmentconfig,
-	"Testing": Testingconfig
+	"Testing": Testingconfig,
+	"Production":Productionconfig
 }
